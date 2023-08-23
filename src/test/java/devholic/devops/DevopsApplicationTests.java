131e5b2c15bfec8bb02a77070682da0f1d64cb0e
devholic22 <hyunjoon.tech@gmail.com>
2023-08-23 17:53:57 +0900

    initial commit

---
+++ src/test/java/devholic/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package devholic.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
